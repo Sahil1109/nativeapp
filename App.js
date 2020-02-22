57c3ba554884f24f3e880562b455063ed7e5bf92
Sahil1109 <sahil.jangra@block8.com>
2020-02-22 21:47:33 +0530

    After Lec14

--- App.js
+++ App.js
@@ -3,44 +3,44 @@ import { StyleSheet, View, FlatList, Text, Alert,TouchableWithoutFeedback, Keybo
 import Header from './components/header'
 import TodoItem from './components/todoItem'
 import AddTodo from './components/addTodo'
+import SandBox from './components/sandBox'
 
-export default function App() {
-  const [todos, setTodos]=useState([
-    {text: 'ASD', key:'1'},
-    {text: 'ZXC', key:'2'},
-    {text: 'QWE', key:'3'}
-  ]);
+export default function App() {const [todos, setTodos]=useState([
+  {text: 'ASD', key:'1'},
+  {text: 'ZXC', key:'2'},
+  {text: 'QWE', key:'3'}
+]);
 
-  const pressHandler =(key)=>{
-    setTodos((prevTodo)=>{
-      return prevTodo.filter(todo => todo.key!= key)
-    })
+const pressHandler =(key)=>{
+  setTodos((prevTodo)=>{
+    return prevTodo.filter(todo => todo.key!= key)
+  })
+}
+
+const submitHandler = (text)=>{
+  if(text.length>3){
+  setTodos((prevTodo)=>{
+    return [
+      {text: text, key:Math.random().toString()},
+      ...prevTodo
+    ];
+  })
+  }
+  else{
+    Alert.alert('OOPS!!','Text length is very small',[
+      {text: 'OK',onPress: ()=> console.log('alert closed')}
+    ]);
   }
 
-  const submitHandler = (text)=>{
-    if(text.length>3){
-    setTodos((prevTodo)=>{
-      return [
-        {text: text, key:Math.random().toString()},
-        ...prevTodo
-      ];
-    })
-    }
-    else{
-      Alert.alert('OOPS!!','Text length is very small',[
-        {text: 'OK',onPress: ()=> console.log('alert closed')}
-      ]);
-    }
-  
-  }
-
+}
   return (
+    // <SandBox></SandBox>
     <TouchableWithoutFeedback onPress={()=>{
       Keyboard.dismiss()
       console.log('dismissed keyboard')
     }}>
       <View style={styles.container}>
-            <Header/>
+          <Header/>
             <View style={styles.content}>
             <AddTodo submitHandler={submitHandler} />
               {/* To form */}
@@ -65,8 +65,12 @@ const styles = StyleSheet.create({
   },
   content:{
     padding:40,
+    // backgroundColor:'skyblue',
+    flex: 1
   },
   list:{
     marginTop: 20,
+    // backgroundColor:'coral',
+    flex:1 
   },
 });
