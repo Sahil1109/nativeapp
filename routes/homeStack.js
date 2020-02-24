820603d1c05bd5d86c83656713295f0a3927b3a0
Sahil1109 <sahil.jangra@block8.com>
2020-02-24 12:06:04 +0530

    After navigation options

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,11 +5,31 @@ import ReviewDetails from '../screens/reviewDetails';
 
 const screens ={
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions:{
+            title: 'GameZone',
+            // headerStyle:{
+            //     backgroundColor: '#777'
+            // }
+        }
     },
     ReviewDetails:{
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions:{
+            title: 'Review Details',
+            // headerStyle:{
+            //     backgroundColor: '#777'
+            // }
+        }
     }
 }
-const HomeStack = createStackNavigator(screens)
+const HomeStack = createStackNavigator(screens,{
+    defaultNavigationOptions:{
+        headerTintColor: '#444',
+        headerStyle:{
+            backgroundColor: '#aaa',
+            height: 80
+        }
+    }
+})
 export default createAppContainer(HomeStack)
